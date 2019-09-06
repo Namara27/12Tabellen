f90b62da40b05366e6edb7873cb03958645b8c5f
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-09-06 14:19:31 +0200

    eerste commit

--- src/Opdracht12_1/Getallen.java
+++ src/Opdracht12_1/Getallen.java
@@ -4,28 +4,28 @@ import java.applet.*;
 import java.awt.*;
 
 public class Getallen extends Applet {
-    double[] salaris;
+    double[] getal;
     double totaal;
 
     public void init() {
-        salaris = new double[10];
-        salaris[0] = 23;
-        salaris[1] = 76;
-        salaris[2] = 54;
-        salaris[3] = 44;
-        salaris[4] = 22;
-        salaris[5] = 69;
-        salaris[6] = 3;
-        salaris[7] = 99;
-        salaris[8] = 17;
-        salaris[9] = 50;
+        getal = new double[10];
+        getal[0] = 23;
+        getal[1] = 76;
+        getal[2] = 54;
+        getal[3] = 44;
+        getal[4] = 22;
+        getal[5] = 69;
+        getal[6] = 3;
+        getal[7] = 99;
+        getal[8] = 17;
+        getal[9] = 50;
     }
 
     public void paint(Graphics g) {
-        for (int teller = 0; teller < salaris.length; teller++) {
-            totaal = salaris[teller];
-            totaal = (salaris[0] + salaris[1] + salaris[2] + salaris[3] + salaris[4] + salaris[5] + salaris[6] + salaris[7] + salaris[8] + salaris[9]) / 10;
-            g.drawString("" + salaris[teller], 50, 20 * teller + 20);
+        for (int teller = 0; teller < getal.length; teller++) {
+            totaal = getal[teller];
+            totaal = (getal[0] + getal[1] + getal[2] + getal[3] + getal[4] + getal[5] + getal[6] + getal[7] + getal[8] + getal[9]) / 10;
+            g.drawString("" + getal[teller], 50, 20 * teller + 20);
             g.drawString("Gemiddelde: " + totaal, 50, 220);
         }
     }
