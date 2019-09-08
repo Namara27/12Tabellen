0cc872f9ef16b638f18567f004f6c39aaca3612d
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-09-08 16:43:16 +0200

    eerste commit

---
+++ src/Opdracht12_3/Tekstvakken.java
@@ -0,0 +1,49 @@
+package Opdracht12_3;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class Tekstvakken extends Applet {
+    Button knop;
+    TextField[] tekstvak;
+    int[] totaal;
+
+    public void init() {
+        tekstvak = new TextField[5];
+        totaal = new int[5];
+
+        for (int i = 0; i < tekstvak.length; i++) {
+            tekstvak[i] = new TextField("");
+            add(tekstvak[i]);
+        }
+        //Knop
+        knop = new Button("Ok");
+        KnopListener kl = new KnopListener();
+        knop.addActionListener(kl);
+        add(knop);
+    }
+
+    public void paint(Graphics g) {}
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+            for (int i = 0; i < tekstvak.length; i++) {
+                String s;
+                s = tekstvak[i].getText();
+                totaal[i] = Integer.parseInt(s);
+            }
+            Arrays.sort(totaal);
+
+            for (int i = 0; i < totaal.length; i++) {
+                tekstvak[i].setText(String.valueOf(totaal[i]));
+            }
+
+            repaint();
+        }
+    }
+}
+
+
